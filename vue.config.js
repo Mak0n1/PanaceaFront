890da278113168db2281ab1be4976b54a0bcec32
Ilya <ilypopov25@gmail.com>
2023-10-31 08:42:30 +0300

    Github Actions

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.PUBLIC_URL || "",
+  publicPath: '/PanaceaFront/',
+  base: '/PanaceaFront/',
   devServer: {
     proxy: 'http://localhost:8080',
-    historyApiFallback: true
-
+    historyApiFallback: true,
   },
   css: {
     loaderOptions: {
