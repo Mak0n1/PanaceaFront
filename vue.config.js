4a4f01dc271ee21698647ff04afeb534a1393395
Ilya <ilypopov25@gmail.com>
2023-11-01 01:44:29 +0300

    Попробуем так

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
       ? '/PanaceaFront/'
       : '/',
   devServer: {
-    proxy: 'http://localhost:8080',
+    proxy: 'https://api.panaceadayz.us/v1',
     historyApiFallback: true
   },
   css: {
