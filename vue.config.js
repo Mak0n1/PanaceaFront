3ab8a8a70053c7c1eaa540e174f7b3c24ac0c02b
Ilya <ilypopov25@gmail.com>
2023-10-31 09:08:07 +0300

    More changes

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.PUBLIC_URL || "",
+  publicPath: process.env.PUBLIC_URL ? "/PanaceaFront/" : "/",
   devServer: {
     proxy: 'http://localhost:8080',
     historyApiFallback: true
