a24292a8c89bb4a379b00bed737324999c4dc8ab
Ilya <ilypopov25@gmail.com>
2023-10-31 08:59:41 +0300

    Change Public Patch

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/PanaceaFront/',
+  publicPath: process.env.PUBLIC_URL || "",
   devServer: {
     proxy: 'http://localhost:8080',
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   css: {
     loaderOptions: {
